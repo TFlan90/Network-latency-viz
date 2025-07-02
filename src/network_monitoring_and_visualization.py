ca14822a8ccf43bcdc3f730c8925a86f8c893fd7
TFlan90 <Tdflan90@gmail.com>
2025-07-02 00:19:49 -0700

    Updated ping function to return  network latency statistics

--- src/network_monitoring_and_visualization.py
+++ src/network_monitoring_and_visualization.py
@@ -1,11 +1,32 @@
 from pythonping import ping
 
+# Returns network latency statistics (min, max, avg RTT, packet loss % ) 
+# and the full ping response object from pythonping
+def getPingStats(ip_or_hostname:str, quantity:int):
+    response = ping(ip_or_hostname, count=quantity)
+    pingDict = {
+                "Min MS Wait" : f"Minimum wait time was {response.rtt_min_ms} ms",
+                "Max MS Wait" : f"Maximum wait time was {response.rtt_max_ms} ms",
+                "Avg MS Wait" : f"Average wait time was {response.rtt_avg_ms} ms",
+                "Packet Loss" : f"Percentage of packets dropped was {response.stats_lost_ratio}%\n"
+                }
+    return pingDict,response
 
-def getPing(ip_or_hostname:str):
-    result = ping(ip_or_hostname, count=4)
-    print(result)
+    
 
 
 
 ip_or_hostname = input("Give an IP address or hostname\n")
-getPing(ip_or_hostname)
+ping_quantity = int(input("How many times should the address be pinged?\n"))
+
+responseStats,responses = (getPingStats(ip_or_hostname,ping_quantity))
+for value in responseStats.values():
+    print(value)
+
+for response in responses:
+    print(response)
+
+
+
+
+
